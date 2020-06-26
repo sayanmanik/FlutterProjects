40fb8d5e8f4741d647d6dccb37898d8b892445e9
sayanmanik <sayanmanik.sm@gmail.com>
2020-06-26 17:26:51 +0530

    added a tabbar to the appbar

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/meal_details_screen.dart';
+import 'package:meals_app/screens/tabs_screen.dart';
 
 import 'screens/categories_screen.dart';
 import 'screens/category_meals_screen.dart';
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
       ),
       //home: CategoriesScreen(),
       routes: {
-        '/':(ctx)=>CategoriesScreen(),
+        '/':(ctx)=>TabsScreen(),
         CategoryMealsScreen.routeName:(ctx)=>CategoryMealsScreen(),
         MealDetailsScreen.routeName:(ctx)=>MealDetailsScreen()
       },
@@ -47,9 +48,6 @@ class MyApp extends StatelessWidget {
           builder: (ctx) => MealDetailsScreen()
         );
       },
-
-
-
       onUnknownRoute: (settings) {
           return MaterialPageRoute(
             builder: (ctx)=> CategoryMealsScreen()
--- meals_app/lib/screens/categories_screen.dart
+++ meals_app/lib/screens/categories_screen.dart
@@ -7,11 +7,7 @@ import 'package:meals_app/widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Deli Meals')
-      ),
-      body: GridView(
+    return  GridView(
         padding: const EdgeInsets.all(25),
       children: DUMMY_CATEGORIES.map((catData) {
         return CategoryItem(catData.id,catData.title, catData.color);
@@ -22,7 +18,6 @@ class CategoriesScreen extends StatelessWidget {
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
-    )
-  );
-}
+    );
+  }
 }
